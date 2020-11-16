7d01265ed4616eb24284ab8dd81c293f806b56f6
pedro <pedro@LAPTOP-QV2UH05P>
2020-11-16 15:22:53 -0300

    super classe inimigo

---
+++ AIIPOO/src/enemies/Inimigos.java
@@ -0,0 +1,37 @@
+package enemies;
+
+public class Inimigos {
+	private int vida;
+	private int atk;
+	private int def;
+	
+	public Inimigos() {
+		
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public int getDef() {
+		return def;
+	}
+
+	public void setDef(int def) {
+		this.def = def;
+	}
+	
+	
+}
