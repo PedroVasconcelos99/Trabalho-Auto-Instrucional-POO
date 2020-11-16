179d016893d7308afa98071d23040ddd06bfbd1f
pedro <pedro@LAPTOP-QV2UH05P>
2020-11-16 16:39:39 -0300

    modificando a forma como os tributos sao atribuidos

--- AIIPOO/src/enemies/GuerreiroEsqueleto.java
+++ AIIPOO/src/enemies/GuerreiroEsqueleto.java
@@ -4,10 +4,12 @@ public class GuerreiroEsqueleto extends Inimigos{
 
 	public GuerreiroEsqueleto(int vida, int atk, int def) {
 		super(vida, atk, def);
-		vida = 300;
-		atk = 10;
-		def = 5;
+		this.vida = vida;
+		this.atk = atk;
+		this.def = def;
 	}
+	
+	
 
 	
 	
--- AIIPOO/src/heroes/Cavaleiro.java
+++ AIIPOO/src/heroes/Cavaleiro.java
@@ -4,11 +4,16 @@ public class Cavaleiro extends Heroi{
 
 	public Cavaleiro(int atk, int def, int vida) {
 		super(atk, def, vida);
-		atk = 20;
-		def = 30;
-		vida = 200;
+		this.atk = atk;
+		this.def = def;
+		this.vida = vida;
 	}
 	
+	public void ataques() {
+		normal(); 
+	}
 	
-
+	public void normal() {
+		
+	}
 }
