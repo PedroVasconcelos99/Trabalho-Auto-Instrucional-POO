f8b09fb9ab90f56d059d0dac0a70ff40e692197a
pedro <pedro@LAPTOP-QV2UH05P>
2020-11-16 15:44:30 -0300

    adicionando um inimigo ao jogo

---
+++ AIIPOO/src/enemies/GuerreiroEsqueleto.java
@@ -0,0 +1,14 @@
+package enemies;
+
+public class GuerreiroEsqueleto extends Inimigos{
+
+	public GuerreiroEsqueleto(int vida, int atk, int def) {
+		super(vida, atk, def);
+		vida = 300;
+		atk = 10;
+		def = 5;
+	}
+
+	
+	
+}
