6607e2a0b37b9c16e9f4ac34aed3494c7769014a
pedro <pedro@LAPTOP-QV2UH05P>
2020-11-16 15:28:02 -0300

    iniciando a classe cavaleiro

---
+++ AIIPOO/src/heroes/Cavaleiro.java
@@ -0,0 +1,14 @@
+package heroes;
+
+public class Cavaleiro extends Heroi{
+
+	public Cavaleiro(int atk, int def, int vida) {
+		super(atk, def, vida);
+		atk = 20;
+		def = 30;
+		vida = 200;
+	}
+	
+	
+
+}
