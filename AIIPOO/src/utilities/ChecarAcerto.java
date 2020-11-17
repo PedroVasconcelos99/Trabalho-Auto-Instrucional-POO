8a9a9d46e08958f5ab7eaec3db97810a9fdaf3eb
pedro <pedro@LAPTOP-QV2UH05P>
2020-11-17 13:05:37 -0300

    criando uma classe para verificar se o ataque acertou, errou, ou foi critico

---
+++ AIIPOO/src/utilities/ChecarAcerto.java
@@ -0,0 +1,14 @@
+package utilities;
+
+public class ChecarAcerto {
+	int check() {
+		int resultado = (int) (Math.random() * (20 - 1 + 1) + 1);
+		if (resultado <=10) {
+			return 1;
+		} else if (resultado <=19) {
+			return 2;
+		} else {
+			return 3;
+		}
+	}
+}
