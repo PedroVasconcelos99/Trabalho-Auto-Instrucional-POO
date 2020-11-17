8974141e9a9502a38ec3ae48e19e5c2afd0f376d
pedro <pedro@LAPTOP-QV2UH05P>
2020-11-17 13:20:05 -0300

    modificando o construtor

--- AIIPOO/src/enemies/GuerreiroEsqueleto.java
+++ AIIPOO/src/enemies/GuerreiroEsqueleto.java
@@ -1,12 +1,12 @@
 package enemies;
 
 public class GuerreiroEsqueleto extends Inimigos{
-
-	public GuerreiroEsqueleto(int vida, int atk, int def) {
-		super(vida, atk, def);
-		this.vida = vida;
-		this.atk = atk;
-		this.def = def;
+	
+	public GuerreiroEsqueleto() {
+		nome = "Skeleton-Warrior";
+		vida = 200;
+		atk = 10;
+		def = 5;
 	}
 	
 	
